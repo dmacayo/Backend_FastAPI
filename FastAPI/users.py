6a04891134713c88303fbdcf5b961f57386593ff
dmacayo <damacayo@gmail.com>
2026-04-14 09:41:15 +0200

    operations post, put, delete

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -36,6 +36,46 @@ async def user(id: int):
 async def user(id: int):
     return search_user(id)
 
+# POST
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+        
+# PUT
+@app.put("/user/")
+async def user(user: User):
+    
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    
+    if not found:
+        return {"error": "No se ha actualizado usuario"}
+    else: return user
+    
+# DELETE
+@app.delete("/user/{id}")
+async def user(id: int):
+    
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index] 
+            found = True
+    
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+    
+        
+
     
 def search_user(id:int):
     users = filter(lambda user: user.id == id, users_list)
@@ -43,3 +83,4 @@ def search_user(id:int):
         return list(users)[0]
     except:
         return {"error": "No se ha encontrado usuario"}
+    
